2eae738c652cb2d0d1c2dacf53982a6e32a08d00
Wolfgang Fahl <wf@bitplan.com>
2020-11-11 15:12:19 +0100

    fixes #4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyThunderbird',
-    version='0.0.3',
+    version='0.0.4',
 
     packages=['thunderbird', ],
     author='Wolfgang Fahl',
