a654fc18d163956bf717b68887b220667b633c80
Wolfgang Fahl <wf@bitplan.com>
2020-11-11 12:39:56 +0100

    adds python 3.9 to setup

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ setup(
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'
     ],
     long_description=long_description,
     long_description_content_type='text/markdown'
