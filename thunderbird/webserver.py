7706d01fd1eb4130af768a1cebc68e5538e6ef99
Wolfgang Fahl <wf@bitplan.com>
2021-10-12 15:20:07 +0200

    adds webserver stub code

--- thunderbird/webserver.py
+++ thunderbird/webserver.py
@@ -4,33 +4,70 @@ Created on 2021-09-23
 @author: wf
 '''
 import os
-from flask import Flask
-from flask import render_template
-import sys
-import argparse
+from flask import render_template, url_for
+from fb4.widgets import MenuItem
+from fb4.app import AppWrap
 
-scriptdir=os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__,static_url_path='',static_folder=scriptdir+'/../web', template_folder=scriptdir+'/../templates')
-#
-@app.route('/')
-def home():
-    return index()
+class WebServer(AppWrap):
+    ''' 
+    Thunderbird Mail webserver
+    '''
+    
+    def __init__(self, host='0.0.0.0', port=8482, verbose=True,debug=False):
+        '''
+        constructor
+        
+        Args:
+            host(str): flask host
+            port(int): the port to use for http connections
+            debug(bool): True if debugging should be switched on
+            verbose(bool): True if verbose logging should be switched on
+            dblp(Dblp): preconfigured dblp access (e.g. for mock testing)
+        '''
+        self.debug=debug
+        self.verbose=verbose
+        self.lookup=None
+        scriptdir = os.path.dirname(os.path.abspath(__file__))
+        template_folder=scriptdir + '/../templates'
+        super().__init__(host=host,port=port,debug=debug,template_folder=template_folder)
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app.app_context().push()
+        
+            
+        @self.app.route('/')
+        def index():
+            return self.index()
+        
+    def getMenuList(self):
+        '''
+        get the list of menu items for the main menu
+        Args:
+            activeItem(str): the active  menu item
+        Return:
+            list: the list of menu items
+        '''
+        menuList=[
+            MenuItem(url_for('index'),'Home'),
+            MenuItem('http://wiki.bitplan.com/index.php/PyThunderbird',"Docs"),
+            MenuItem('https://github.com/WolfgangFahl/pyThunderbird','github'),
+            ]
+        return menuList
 
-def index():
-    """ render index page with the given parameters"""
-    return render_template('index.html')
+    def index(self):
+        """ render index page with the given parameters"""
+        return render_template('index.html',menuList=self.getMenuList())
 
+def main():
+    '''
+    main entry for Webserver
+    '''
+    web=WebServer()
+    parser=web.getParser(description="Thunderbird mail webservice")
+    parser.add_argument('--verbose',default=True,action="store_true",help="should relevant server actions be logged [default: %(default)s]")
+    args=parser.parse_args()
+    web.optionalDebug(args)
+    web.verbose=args.verbose
+    web.run(args)
+    
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Thunderbird Mail Webservice")
-    parser.add_argument('--debug',
-                                 action='store_true',
-                                 help="run in debug mode")
-    parser.add_argument('--port',
-                                 type=int,
-                                 default=8482,
-                                 help="the port to use")
-    parser.add_argument('--host',
-                                 default="0.0.0.0",
-                                 help="the host to serve for")
-    args=parser.parse_args(sys.argv[1:])
-    app.run(debug=args.debug,port=args.port,host=args.host)
+    main()
